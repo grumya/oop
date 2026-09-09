d2bbfe2be0afc6c55bcb41a70345ca2730c539fc
Maria Safonova <m.safonova1@g.nsu.ru>
2026-09-09 20:49:59 +0700

    Увеличение покрытия

--- Task_1_1/src/main/java/Main.java
+++ Task_1_1/src/main/java/Main.java
@@ -4,8 +4,7 @@ public class Main {
     public static void main(String[] args) {
         int[] array = {3, 2, 1};
 
-        HeapSort heapSort = new HeapSort();
-        heapSort.heapsort(array);
+        HeapSort.heapsort(array);
 
         System.out.println(Arrays.toString(array));
     }
