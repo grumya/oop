0861f371b94d889ae6e463538bbf169cd4c82975
Maria Safonova <m.safonova1@g.nsu.ru>
2026-09-09 21:05:26 +0700

    Увеличение покрытия

--- Task_1_1/src/main/java/HeapSort.java
+++ Task_1_1/src/main/java/HeapSort.java
@@ -3,6 +3,8 @@
  */
 public class HeapSort {
 
+    private HeapSort() {
+    }
     /**
      * сортирует массив по возрастанию с помощью пирамиды
      *
@@ -12,20 +14,16 @@ public class HeapSort {
     public static int[] heapsort(int[] array) {
         int n = array.length;
 
-        // строим максимальную кучу
         for (int i = n / 2 - 1; i >= 0; i--) {
             fixHeap(array, n, i);
         }
 
-        // переносим максимальные элементы в конец массива
         for (int i = n - 1; i > 0; i--) {
 
-            // меняем первый и последний элементы кучи местами
             int tmp = array[0];
             array[0] = array[i];
             array[i] = tmp;
 
-            // восстанавливаем кучу
             fixHeap(array, i, 0);
         }
 
@@ -41,34 +39,27 @@ public class HeapSort {
      */
     private static void fixHeap(int[] arr, int size, int i) {
         while (true) {
-            // считаем текущий элемент максимальным
             int biggest = i;
 
-            // находим индексы потомков
             int left = 2 * i + 1;
             int right = 2 * i + 2;
 
-            // проверяем левого потомка
             if (left < size && arr[left] > arr[biggest]) {
                 biggest = left;
             }
 
-            // проверяем правого потомка
             if (right < size && arr[right] > arr[biggest]) {
                 biggest = right;
             }
 
-            // если текущий элемент уже максимальный, заканчиваем
             if (biggest == i) {
                 break;
             }
 
-            // меняем текущий элемент с максимальным потомком
             int tmp = arr[i];
             arr[i] = arr[biggest];
             arr[biggest] = tmp;
 
-            // продолжаем проверку ниже
             i = biggest;
         }
     }
